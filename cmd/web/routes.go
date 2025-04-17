97001dbd5c8c0d1bf8f6f184e59528031f417644
Abdulsamed Arslan <samedarslan0723@gmail.com>
2025-04-17 17:41:07 +0300

    - middleware.go file added for handle middlewares in project. - secure headers, infoLogger and panic revocery middlewares added. - justinas/alice v1.2.0 package added for chaining middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
@@ -11,5 +14,8 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	// Apply a chain of middleware
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(mux)
 }
