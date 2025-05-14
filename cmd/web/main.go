c5e8ad49de3679dd187cc435614d29580132904e
Abdulsamed Arslan <samedarslan0723@gmail.com>
2025-05-15 02:30:00 +0300

    refactor: CSRF token naming fix on nav.gohtml

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -63,7 +63,7 @@ func main() {
 	// Setting this means that the cookie will only be sent by a user's web
 	// browser when an HTTPS connection is being used (and won't be sent over an
 	// unsecure HTTP connection).
-	sessionManager.Cookie.Secure = false
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		errorLog:       errorLog,
